ed4c5daa902e66cca44da44a85eaa96bbde34f04
RobertOltean314 <olteanrobertadrian314@gmail.com>
2024-08-28 17:07:50 +0300

    Add project files.

---
+++ 04_Cookies_Recipe_Assigment/Program.cs
@@ -0,0 +1,22 @@
+﻿using _04_Cookies_Recipe_Assigment.DataAccess;
+using _04_Cookies_Recipe_Assigment.FileAccess;
+
+const FileFormat Format = FileFormat.Json;
+
+IStringsRespository stringsRepository = Format == FileFormat.Json
+    ? new StringsJsonRespository()
+    : new StringsTextualRespository();
+
+const string FileName = "recipes";
+var fileMetaData = new FileMetaData(FileName, Format);
+
+var ingredientsRegister = new IngredientsRegister();
+
+var cookiesRecipeApp = new CookiesRecipeApp(
+    new RecipeReopository(stringsRepository, ingredientsRegister),
+    new RecipeConsoleUserInteraction(ingredientsRegister));
+
+cookiesRecipeApp.Run(fileMetaData.toPath());
+
+
+

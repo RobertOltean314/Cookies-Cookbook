a69e09118e070d2e454530880629b19f7a35a318
RobertOltean314 <olteanrobertadrian314@gmail.com>
2024-08-31 07:32:51 +0300

    Added a global try catch

--- 04_Cookies_Recipe_Assigment/Program.cs
+++ 04_Cookies_Recipe_Assigment/Program.cs
@@ -1,22 +1,30 @@
 ﻿using _04_Cookies_Recipe_Assigment.DataAccess;
 using _04_Cookies_Recipe_Assigment.FileAccess;
 
-const FileFormat Format = FileFormat.Json;
+try
+{
+    const FileFormat Format = FileFormat.Json;
 
-IStringsRespository stringsRepository = Format == FileFormat.Json
-    ? new StringsJsonRespository()
-    : new StringsTextualRespository();
+    IStringsRespository stringsRepository = Format == FileFormat.Json
+        ? new StringsJsonRespository()
+        : new StringsTextualRespository();
 
-const string FileName = "recipes";
-var fileMetaData = new FileMetaData(FileName, Format);
+    const string FileName = "recipes";
+    var fileMetaData = new FileMetaData(FileName, Format);
 
-var ingredientsRegister = new IngredientsRegister();
+    var ingredientsRegister = new IngredientsRegister();
 
-var cookiesRecipeApp = new CookiesRecipeApp(
-    new RecipeReopository(stringsRepository, ingredientsRegister),
-    new RecipeConsoleUserInteraction(ingredientsRegister));
+    var cookiesRecipeApp = new CookiesRecipeApp(
+        new RecipeReopository(stringsRepository, ingredientsRegister),
+        new RecipeConsoleUserInteraction(ingredientsRegister));
 
-cookiesRecipeApp.Run(fileMetaData.toPath());
-
-
-
+    cookiesRecipeApp.Run(fileMetaData.toPath());
+}
+catch (Exception ex)
+{
+    Console.WriteLine("We're sorry, the application expected" +
+        "an unexpected error and will be closed." +
+        "The error message: " + ex.Message);
+    Console.WriteLine("Press any key to close the application.");
+    Console.ReadKey();
+}
